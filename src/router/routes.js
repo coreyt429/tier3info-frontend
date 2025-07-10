d4f2cb1b27a495e39e55b815cbbe3d172a37d69b
Corey Thornburg <coreyt@coreyt.com>
2025-07-10 03:07:56 -0500

    test_202507100307

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,7 @@
+import { h } from 'vue'
+import { Suspense } from 'vue'
+import ConfigPage from 'pages/ConfigPage.vue'
+
 const routes = [
   {
     path: '/',
@@ -11,15 +15,31 @@ const routes = [
   },
   {
     path: '/config',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      {
-        path: '',
-        component: () => import('pages/ConfigPage.vue'),
-        meta: { title: 'Tier3info Configuration', endpoint: '/cfg/' },
+    component: {
+      render() {
+        return h(Suspense, null, {
+          default: h(ConfigPage),
+          fallback: h('div', 'Loading...'),
+        })
       },
-    ],
+    },
+    meta: {
+      title: 'Config Page',
+      label: 'Configuration',
+      endpoint: '/cfg',
+    },
   },
+  //   {
+  //   path: '/config',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     {
+  //       path: '',
+  //       component: () => import('pages/ConfigPage.vue'),
+  //       meta: { title: 'Tier3info Configuration', endpoint: '/cfg/' },
+  //     },
+  //   ],
+  // },
   // Always leave this as last one,
   // but you can also remove it
   {
